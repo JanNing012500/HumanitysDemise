c707276ce841320ccc149507496c5b96868900ff
Ryan Chen <rchen73@ucsc.edu>
2021-10-13 17:51:46 -0700

    cleaned

--- docs/_template/main.js
+++ docs/_template/main.js
@@ -1,52 +1,63 @@
-title = "BLITZED";
+title = "HUMANITY'S DEMISE";
 
 description = `
-Annihilate.
-[Hold Left]
-  Move Left
-[Hold Right]
-  Move Right
+  Destroy Humans!
+
+      [Mouse]
+Move Left and Right
+
+     [Hold LMB]
+    Shoot Missile
 `;
 
 characters = [
 `
   ll
-  ll 
- llll 
+  ll
  llll
 llllll
 llllll
+  ll
 `,`
  l
-lll
+ l
  l
 `,`
+  ll
 llllll
 l ll l
  llll
  l  l
+ l  l
 `,`
+  ll
  llll
 l ll l
  llll
 l    l
+l    l
 `,`
- ll
-l  l
-l  l
- ll
+ llll
+ llll
 `,
 `
+  ll
  llll
 llllll
-l ll l
 llllll
-ll  ll
+llllll
+`,`
+  ll
+  ll
+llllll
+llllll
+  ll
+llllll
 `
 ];
 
 const G = {
-    WIDTH: 100,
+    WIDTH: 120,
     HEIGHT: 150,
     OUTER_BORDER: 30,
 
@@ -73,7 +84,7 @@ const G = {
 
 options = {
     viewSize: {x: G.WIDTH, y: G.HEIGHT},
-    theme: "pixel",
+    theme: "crt",
     isDrawingParticleFront: true,
     isDrawingScoreFront: true,
     isPlayingBgm: true,
@@ -174,6 +185,11 @@ let enemyFiringCooldown;
 let waveCount;
 
 /**
+ * @type { Player }
+ */
+let player2;
+
+/**
  * @type { Color[] }
  */
  const C = [
@@ -207,83 +223,83 @@ let shotX;
 let shotSize;
 let charge;
 
-function shootall()
- {fBullets.push({
-    pos: vec(5, 160)
+function shootall() {
+    fBullets.push({
+        pos: vec(5, 160)
     });
 
     fBullets.push({
         pos: vec(10, 160)
-        });
+    });
 
-        fBullets.push({
-            pos: vec(15, 160)
-            });
+    fBullets.push({
+        pos: vec(15, 160)
+    });
 
-            fBullets.push({
-                pos: vec(20, 160)
-                });
+    fBullets.push({
+        pos: vec(20, 160)
+    });
 
-                fBullets.push({
-                    pos: vec(25, 160)
-                    });
+    fBullets.push({
+        pos: vec(25, 160)
+    });
 
-                    fBullets.push({
-                        pos: vec(30, 160)
-                        });
-                        fBullets.push({
-                            pos: vec(35, 160)
-                            });
+    fBullets.push({
+        pos: vec(30, 160)
+    });
+    fBullets.push({
+        pos: vec(35, 160)
+    });
                 
-                            fBullets.push({
-                                pos: vec(40, 160)
-                                });
+    fBullets.push({
+        pos: vec(40, 160)
+    });
                 
-                                fBullets.push({
-                                    pos: vec(45, 160)
-                                    });
+    fBullets.push({
+        pos: vec(45, 160)
+    });
                 
-                                    fBullets.push({
-                                        pos: vec(50, 160)
-                                        });
+    fBullets.push({
+        pos: vec(50, 160)
+    });
                 
-                                        fBullets.push({
-                                            pos: vec(55, 160)
-                                            });
+    fBullets.push({
+        pos: vec(55, 160)
+    });
                 
-                                            fBullets.push({
-                                                pos: vec(60, 160)
-                                                });
-                                                fBullets.push({
-                                                    pos: vec(65, 160)
-                                                    });
+    fBullets.push({
+        pos: vec(60, 160)
+    });
+    fBullets.push({
+        pos: vec(65, 160)
+    });
                                         
-                                                    fBullets.push({
-                                                        pos: vec(70, 160)
-                                                        });
+    fBullets.push({
+        pos: vec(70, 160)
+    });
                                         
-                                                        fBullets.push({
-                                                            pos: vec(75, 160)
-                                                            });
+    fBullets.push({
+        pos: vec(75, 160)
+    });
                                         
-                                                            fBullets.push({
-                                                                pos: vec(80, 160)
-                                                                });
+    fBullets.push({
+        pos: vec(80, 160)
+    });
                                         
-                                                                fBullets.push({
-                                                                    pos: vec(85, 160)
-                                                                    });
+    fBullets.push({
+        pos: vec(85, 160)
+    });
                                         
-                                                                    fBullets.push({
-                                                                        pos: vec(90, 160)
-                                                                        });
-                                                                        fBullets.push({
-                                                                            pos: vec(95, 160)
-                                                                            });
-                                                                            fBullets.push({
-                                                                                pos: vec(100, 160)
-                                                                                });
-                                                                            }
+    fBullets.push({
+        pos: vec(90, 160)
+    });
+    fBullets.push({
+        pos: vec(95, 160)
+    });
+    fBullets.push({
+        pos: vec(100, 160)
+    });
+}
 
 
 var bigbulletcount=5; //player starts off with 5 big shots
@@ -295,10 +311,19 @@ function update() {
             isFiring: false,
             firingCooldown: 0
         };
+
+        player2 = {
+            pos: vec(G.WIDTH*0.5 + 5, G.HEIGHT*0.9),
+            life:G.PLAYER_LIFE,
+            isFiring: false,
+            firingCooldown: 0
+        }
+
         fBullets = [];
         bigBullets = [];
         enemies = [];
         eBullets = [];
+
         stars = times(rndi(4, 7), () => {
             return {
                 pos: vec(rnd(G.WIDTH), rnd(G.HEIGHT)),
@@ -318,16 +343,15 @@ function update() {
 
         shotX = shotSize = undefined;
         charge = 0;
-    
-  }
+    }
   
     if(G.PLAYER_LIFE<0)
     {
-      end();
-      G.PLAYER_LIFE=50;
-      G.PLAYER_FIRE_RATE =30;
-      G.ENEMY_HP = 1;
-      bigbulletcount = 5;
+        end();
+        G.PLAYER_LIFE=50;
+        G.PLAYER_FIRE_RATE =30;
+        G.ENEMY_HP = 1;
+        bigbulletcount = 5;
     }
 
     if (enemies.length === 0) {
@@ -352,62 +376,58 @@ function update() {
     // });
 
     color(currentCol.fg);
-    char("a", player.pos);
+    char("g", player.pos);
+    char("a", player2.pos);
     player.firingCooldown--;
     if (player.firingCooldown < 0 && !input.isPressed) {
         player.firingCooldown = G.PLAYER_FIRE_RATE;
 
-
-      //muzzle flash
-      particle(
-      player.pos.x,  // x coordinate
-      player.pos.y, // y coordinate
-      5, // The number of particles
-      1, // The speed of the particles
-      -PI/2, // The emitting angle
-      PI/4  // The emitting width
-      );
-
+        //muzzle flash
+        particle(
+            player.pos.x,  // x coordinate
+            player.pos.y, // y coordinate
+            5, // The number of particles
+            1, // The speed of the particles
+            -PI/2, // The emitting angle
+            PI/4  // The emitting width
+        );
 
         fBullets.push({ pos: vec(player.pos.x, player.pos.y) });
-      
     } else if (player.firingCooldown < 0 && input.isPressed && bigbulletcount>0) {
         player.firingCooldown = 30;
 
         bigbulletcount--;
 
-
-      //muzzle flash
-      particle(
-      player.pos.x,  // x coordinate
-      player.pos.y, // y coordinate
-      5, // The number of particles
-      1, // The speed of the particles
-      -PI/2, // The emitting angle
-      PI/4  // The emitting width
-      );
-
+        //muzzle flash
+        particle(
+            player.pos.x,  // x coordinate
+            player.pos.y, // y coordinate
+            5, // The number of particles
+            1, // The speed of the particles
+            -PI/2, // The emitting angle
+            PI/4  // The emitting width
+        );
 
         bigBullets.push({ pos: vec(player.pos.x, player.pos.y) });
     }
 
-
-  //Player Engine Particles
+    //Player Engine Particles
   
-  particle(
-  player.pos.x,  // x coordinate
-  player.pos.y, // y coordinate
-  5, // The number of particles
-  5, // The speed of the particles
-  PI/2, // The emitting angle (originally 2)
-  PI/5  // The emitting width
-  );
+    particle(
+        player.pos.x,  // x coordinate
+        player.pos.y, // y coordinate
+        5, // The number of particles
+        5, // The speed of the particles
+        PI/2, // The emitting angle (originally 2)
+        PI/5  // The emitting width
+    );
  
     //MOVEMENT
     player.pos = vec(input.pos.x, G.HEIGHT * 0.9);
     player.pos.x = clamp(player.pos.x, G.WIDTH * 0.1, G.WIDTH * 0.9);
+    player2.pos = vec(input.pos.x, 129);
+    player2.pos.x = clamp(player.pos.x, G.WIDTH * 0.1, G.WIDTH * 0.9);
     ///
-
 
     fBullets.forEach((fb) => {
         fb.pos.y -= G.FBULLET_SPEED;
@@ -417,7 +437,7 @@ function update() {
     bigBullets.forEach((bb) => {
         bb.pos.y -= G.BBULLET_SPEED;
         char("f", bb.pos);
-    } )
+    });
 
     enemyFiringCooldown--;
     if (enemyFiringCooldown <= 0) {
@@ -444,7 +464,6 @@ function update() {
     }
 
     remove(enemies, (e) => {
-        
         switch(e.state) {
             case EnemyState.LEFT:
                 e.pos.x -= e.speed;
@@ -458,34 +477,23 @@ function update() {
         }
 
         e.distanceLog += abs(e.speed);
-        if (e.state === EnemyState.LEFT
-            && e.distanceLog >= G.ENEMY_TRIGGER_DISTANCE_HORIZONTAL) {
-
+        if (e.state === EnemyState.LEFT && e.distanceLog >= G.ENEMY_TRIGGER_DISTANCE_HORIZONTAL) {
             e.distanceLog = 0;
             e.state = EnemyState.DOWN;
             e.nextDir = EnemyState.RIGHT;
-            e.speed = G.ENEMY_MOVE_SPD_VERTICAL
-                + difficulty * G.DIFFICULTY_MODIFIER;
-        } else if (e.state === EnemyState.RIGHT
-            && e.distanceLog >= G.ENEMY_TRIGGER_DISTANCE_HORIZONTAL) {
-
+            e.speed = G.ENEMY_MOVE_SPD_VERTICAL + difficulty * G.DIFFICULTY_MODIFIER;
+        } else if (e.state === EnemyState.RIGHT && e.distanceLog >= G.ENEMY_TRIGGER_DISTANCE_HORIZONTAL) {
             e.distanceLog = 0;
             e.state = EnemyState.DOWN;
             e.nextDir = EnemyState.LEFT;
-            e.speed = G.ENEMY_MOVE_SPD_VERTICAL
-                + difficulty * G.DIFFICULTY_MODIFIER;
-        } else if (e.state === EnemyState.DOWN
-            && e.distanceLog >= G.ENEMY_TRIGGER_DISTANCE_VERTICAL) {
-
+            e.speed = G.ENEMY_MOVE_SPD_VERTICAL + difficulty * G.DIFFICULTY_MODIFIER;
+        } else if (e.state === EnemyState.DOWN && e.distanceLog >= G.ENEMY_TRIGGER_DISTANCE_VERTICAL) {
             e.distanceLog = 0;
             e.state = e.nextDir;
-            e.speed = G.ENEMY_MOVE_SPD_HORIZONTAL
-                + difficulty * G.DIFFICULTY_MODIFIER;
+            e.speed = G.ENEMY_MOVE_SPD_HORIZONTAL + difficulty * G.DIFFICULTY_MODIFIER;
         }
         
-        
-        const isCollidingWithBigBullet =char(addWithCharCode("c", floor(ticks/G.ENEMY_ANIM_SPD)%2), e.pos)
-        .isColliding.char.f;
+        const isCollidingWithBigBullet =char(addWithCharCode("c", floor(ticks/G.ENEMY_ANIM_SPD)%2), e.pos).isColliding.char.f;
         
         if(isCollidingWithBigBullet){
             //color("yellow");
@@ -493,13 +501,9 @@ function update() {
             e.hp=e.hp-20;
         }
 
-        const isCollidingWithPlayer =
-            char(addWithCharCode("c", floor(ticks/G.ENEMY_ANIM_SPD)%2), e.pos)
-                .isColliding.char.a;
+        const isCollidingWithPlayer = char(addWithCharCode("c", floor(ticks/G.ENEMY_ANIM_SPD)%2), e.pos).isColliding.char.a;
         
-                const isCollidingWithFBullet =  
-            char(addWithCharCode("c", floor(ticks/G.ENEMY_ANIM_SPD)%2), e.pos)
-                .isColliding.char.b;
+        const isCollidingWithFBullet = char(addWithCharCode("c", floor(ticks/G.ENEMY_ANIM_SPD)%2), e.pos).isColliding.char.b;
 
         if (isCollidingWithFBullet) e.hp--;
         if (isCollidingWithPlayer) {
@@ -518,26 +522,22 @@ function update() {
 
         return (e.hp<0||e.hp === 0 || e.pos.y > G.HEIGHT);
     });
-    
- 
 
     remove(eBullets, (eb) => {
         eb.pos.x += G.EBULLET_SPEED * Math.cos(eb.angle);
         eb.pos.y += G.EBULLET_SPEED * Math.sin(eb.angle);
-      
         
         const isCollidingWithFBullet = char("e", eb.pos).isColliding.char.b; //remove enemy bullets if collides with player bullet
-        if(isCollidingWithFBullet){
+        if (isCollidingWithFBullet){
             play("hit");
             particle(eb.pos) //destroys enemy bullet 
         }
         
         const isCollidingWithBigBullet =char("e",eb.pos).isColliding.char.f; //removes enemy bullet if it touches bit bullet
-        if(isCollidingWithBigBullet){
+        if (isCollidingWithBigBullet){
             play("hit");
             particle(eb.pos) //destroys enemy bullet 
         }
-
 
         const isCollidingWithPlayer = char("e", eb.pos).isColliding.char.a;
         if (isCollidingWithPlayer) {
@@ -545,38 +545,26 @@ function update() {
             G.PLAYER_LIFE--;
             shootall();
             play("lucky");
-          
         }
 
         return (isCollidingWithBigBullet|| isCollidingWithFBullet||!eb.pos.isInRect(0, 0, G.WIDTH, G.HEIGHT));
     });
 
-
-
-
     remove(fBullets, (fb) => { //removing player bullets
-       
-      
-
-      const isCollidingWithEnemy =
-            char("b", fb.pos).isColliding.char.c
-            || char("b", fb.pos).isColliding.char.d;
+      const isCollidingWithEnemy = char("b", fb.pos).isColliding.char.c || char("b", fb.pos).isColliding.char.d;
 
         if (isCollidingWithEnemy) {
             play("hit");
             particle(fb.pos);
         }
 
-        const isCollidingwithEBullets =   //removes player bullet when collides with enemy bullet
-        char("b", fb.pos).isColliding.char.e;
+        const isCollidingwithEBullets = char("b", fb.pos).isColliding.char.e; //removes player bullet when collides with enemy bullet
          
           if (isCollidingwithEBullets) {
             play("hit"); 
             color("red");           
             particle(fb.pos);
-            
         }
-
     
         return (isCollidingwithEBullets||isCollidingWithEnemy || fb.pos.y < 0);
     });
@@ -591,61 +579,53 @@ function update() {
     text("MISSLES:"+bigbulletcount.toString(),3,20);
 
     function regenerate() {
-
-
-        if(G.ENEMY_HP>=4)  //figure out how to channge enemy sprite
-        {
+        if(G.ENEMY_HP>=4) { //figure out how to channge enemy sprite
           currentCol.fg = (C[rndi(C.length)]);
           currentCol.bg = (C[rndi(C.length)]);
           do {
-              currentCol.bg = (C[rndi(C.length)]);
+            currentCol.bg = (C[rndi(C.length)]);
           } while (currentCol.fg === currentCol.bg);
           
           for (let i = 0; i < 5; i++) {
               for (let j = 0; j < 4; j++) {
-  
                   let x = G.WIDTH*0.1 + i*12 + (j%2)*6;
                   let y = G.HEIGHT*0.1 + j*6;
   
                   enemies.push({
-                      pos: vec(x, y),
-                      hp: G.ENEMY_HP + ceil(waveCount/2),
-                      state: EnemyState.RIGHT,
-                      nextDir: EnemyState.LEFT,
-                      speed: G.ENEMY_MOVE_SPD_HORIZONTAL
-                          + difficulty * G.DIFFICULTY_MODIFIER,
-                      distanceLog: 0
-                  });
-              }
-          }
-        }
-        
-        else{
-        
-        currentCol.fg = (C[rndi(C.length)]);
-        currentCol.bg = (C[rndi(C.length)]);
-        do {
-            currentCol.bg = (C[rndi(C.length)]);
-        } while (currentCol.fg === currentCol.bg);
-        
-        for (let i = 0; i < 5; i++) {
-            for (let j = 0; j < 4; j++) {
-
-                let x = G.WIDTH*0.1 + i*12 + (j%2)*6;
-                let y = G.HEIGHT*0.1 + j*6;
-
-                enemies.push({
-                    pos: vec(x, y),
-                    hp: G.ENEMY_HP + ceil(waveCount/2),
-                    state: EnemyState.RIGHT,
-                    nextDir: EnemyState.LEFT,
-                    speed: G.ENEMY_MOVE_SPD_HORIZONTAL
-                        + difficulty * G.DIFFICULTY_MODIFIER,
-                    distanceLog: 0
-                });
+                        pos: vec(x, y),
+                        hp: G.ENEMY_HP + ceil(waveCount/2),
+                        state: EnemyState.RIGHT,
+                        nextDir: EnemyState.LEFT,
+                        speed: G.ENEMY_MOVE_SPD_HORIZONTAL + difficulty * G.DIFFICULTY_MODIFIER,
+                        distanceLog: 0
+                    });
+                }
             }
         }
-      }
+        
+        else {
+            currentCol.fg = (C[rndi(C.length)]);
+            currentCol.bg = (C[rndi(C.length)]);
+            do {
+                currentCol.bg = (C[rndi(C.length)]);
+            } while (currentCol.fg === currentCol.bg);
+            
+            for (let i = 0; i < 5; i++) {
+                for (let j = 0; j < 4; j++) {
+                    let x = G.WIDTH*0.1 + i*12 + (j%2)*6;
+                    let y = G.HEIGHT*0.1 + j*6;
+
+                    enemies.push({
+                        pos: vec(x, y),
+                        hp: G.ENEMY_HP + ceil(waveCount/2),
+                        state: EnemyState.RIGHT,
+                        nextDir: EnemyState.LEFT,
+                        speed: G.ENEMY_MOVE_SPD_HORIZONTAL + difficulty * G.DIFFICULTY_MODIFIER,
+                        distanceLog: 0
+                    });
+                }
+            }
+        }
         
         enemyFiringCooldown = G.ENEMY_FIRE_RATE - difficulty * 0.1;
         G.ENEMY_HP++;
@@ -655,15 +635,13 @@ function update() {
 
     //stars
     stars.forEach((s) => {
-    
-    //var starcolors= ["yellow","white"];
-    //var starcolor = starcolors[Math.floor(Math.random()*starcolors.length)];
-    // @ts-ignore
-    //color(starcolor);      
-    s.pos.y += s.speed*(difficulty);  
-    if (s.pos.y > G.HEIGHT) s.pos.y = 0;
-    color("light_black");       
-    box(s.pos, 1);
-    
+        //var starcolors= ["yellow","white"];
+        //var starcolor = starcolors[Math.floor(Math.random()*starcolors.length)];
+        // @ts-ignore
+        //color(starcolor);      
+        s.pos.y += s.speed*(difficulty);  
+        if (s.pos.y > G.HEIGHT) s.pos.y = 0;
+        color("light_black");       
+        box(s.pos, 1);
     });
 }
